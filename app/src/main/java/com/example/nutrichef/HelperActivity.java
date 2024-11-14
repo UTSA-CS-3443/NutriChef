2bc140e7d31d7472ce48c4420d457ed519ac45ec
BTCMauxri <mauriciov4795@gmail.com>
2024-11-14 14:09:49 -0600

    Helper Page Changes(Mauricio)

--- app/src/main/java/com/example/nutrichef/HelperActivity.java
+++ app/src/main/java/com/example/nutrichef/HelperActivity.java
@@ -1,6 +1,7 @@
 package com.example.nutrichef;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,12 +18,13 @@ public class HelperActivity extends AppCompatActivity {
         TextView helperView = findViewById(R.id.helperView);
 
 
-        String helpText = "Usage Instructions:\n\n" +
-                "App Functionalities:\n" +
-                "- **Select Recipes by Category**: Navigate through different categories (Appetizer, Entrée, Dessert) using the buttons at the bottom of the screen to explore various recipes.\n" +
-                "\n- **Add New Dish**: Tap the '+' icon at the bottom of the screen. You will be prompted to enter the Dish Name, Ingredients, Instructions, and Nutrition Info.\n" +
-                "\n- **Remove Dish**: To delete a dish, select it from your recipe list. On the selected page, click the 'Remove Dish' button in the bottom left corner. A confirmation prompt will appear, as this action is irreversible.\n" +
-                "\n- **Modify Dish**: Choose the dish you wish to edit. On the selected page, click the 'Modify Dish' button in the bottom right corner. This allows you to update Ingredients, Instructions, or Nutrition Info. Confirm the changes when prompted.\n";
-        helperView.setText(helpText);
+            String helpText = "<br>" +
+                    "<b>App Functionalities:</b><br>" +
+                    "- <b>Select Recipes by Category</b>: Navigate through different categories (Appetizer, Entrée, Dessert) using the buttons at the bottom of the screen to explore various recipes.<br><br>" +
+                    "- <b>Add New Dish</b>: Tap the '+' icon at the bottom of the screen. You will be prompted to enter the Dish Name, Ingredients, Instructions, and Nutrition Info.<br><br>" +
+                    "- <b>Remove Dish</b>: To delete a dish, select it from your recipe list. On the selected page, click the 'Remove Dish' button in the bottom left corner. A confirmation prompt will appear, as this action is irreversible.<br><br>" +
+                    "- <b>Modify Dish</b>: Choose the dish you wish to edit. On the selected page, click the 'Modify Dish' button in the bottom right corner. This allows you to update Ingredients, Instructions, or Nutrition Info. Confirm the changes when prompted.<br>";
+
+            helperView.setText(Html.fromHtml(helpText));
     }
 }
